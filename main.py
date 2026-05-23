526b65b4378273e54678e718d8a9eba8e2714510
Juan R <juanabarro@protonmail.com>
2026-05-23 09:51:40 -0400

    client credentials added

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 from fastapi import FastAPI
+import auth.token_manager
+
+auth_manager = auth.token_manager.auth_manager
+sp = auth.token_manager.sp
+
+print(auth_manager)
+print(sp)
 
 app = FastAPI()
 
 @app.get("/")
 def read_root():
     return {"message": "Playlist Reliability Service is running"}
+
+# @app.get("/playlist/{playlist_id}/analysis")
+
+# @app.get("/playlist/{playlist_id}/clean")
 
